669107bf0b17feeef796287945c25f91f2eaafe9
Ruben Granet <ruben.granet@icloud.com>
2022-12-10 22:08:49 +0200

    Initial Commit

---
+++ Monnshot/MonnshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonnshotApp.swift
+//  Monnshot
+//
+//  Created by Ruben Granet on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MonnshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
